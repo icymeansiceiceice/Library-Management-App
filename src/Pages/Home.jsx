bc8c73c6d609c9e06a5d3db25ef7757ea71e64e1
nOttOD4y <kaungmyat97k@gmail.com>
2023-11-01 22:48:44 +0630

    add react tranction

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,30 @@
+import HeroSection from "../Component/HeroSection"
+import book from '../assets/the-rest-is-history-hardback-book-jacket.jpg'
+
 
 function Home() {
 
   return (
     <>
-      <div className='text-blue-500'>
-            hello
+      <HeroSection/>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 my-3">
+          {
+            [1,2,3,4,5].map(()=> (
+              <div className="p-4 border border-1">
+                <img src={book} alt=""/>
+                <div className="text-center space-y-2 mt-3">
+                  <h1>Book title</h1>
+                  <p>description</p>
+                  <div className="flex flex-wrap">
+                     {['travel','knowledge','triller,super'].map((g)=>(
+                        <span className="mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500">{g}</span>
+                     ))
+                     }
+                  </div>
+                </div>
+              </div>
+            ))
+          }
       </div>  
     </>
   )
--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from '../Component/Navbar'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import './style.css'
 
 
 export default function Layout() {
+  let location = useLocation();
   return (
     <div>
        <Navbar/>
-        <div className='max-w-6xl mx-auto p-3'>
-          <Outlet/>
-        </div>
-    </div>
+       <SwitchTransition>
+          <CSSTransition timeout={200} classNames='fade' key={location.pathname}>
+            <div className='max-w-6xl mx-auto p-3'>
+              <Outlet/>
+            </div>
+          </CSSTransition>
+       </SwitchTransition>
+       </div>  
   )
 }
